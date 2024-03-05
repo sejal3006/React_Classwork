9a660b5c0b33a36ee8f027a32715a2e2f39a51a0
Sejal <sejalrathod.3002@gmail.com>
2024-03-05 20:01:29 +0530

    Added react file

---
+++ script.js
@@ -0,0 +1,18 @@
+function LearningArea() {
+    return (
+      React.createElement('div', null,
+        React.createElement('h1', { style: { fontSize: '30px', fontFamily: 'Algerian' } }, 'LEARN WEB DEVELOPMENT'),
+        React.createElement('p', { style: { fontSize: '20px', fontFamily: 'Georgia' } }, 'Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.'),
+        React.createElement('p', { style: { fontSize: '20px', fontFamily: 'Georgia' } }, 'The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from ',
+          React.createElement('a', { href: 'https://developer.mozilla.org/', target: '_blank' }, 'the rest of MDN'),
+          ', and other intermediate to advanced resources that assume a lot of previous knowledge.'),
+        React.createElement('p',{ style: { fontSize: '20px', fontFamily: 'Georgia' } }, 'If you are a complete beginner, web development can be challenging —— we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.')
+      )
+    );
+  }
+  
+  ReactDOM.render(
+    React.createElement(LearningArea),
+    document.getElementById('root')
+  );
+  
